ed65d58c318ce76adda3aaffb1c06561072928d9
laura13usero <laura13usero@gmail.com>
2025-03-29 17:12:59 +0100

    update RDS

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',        // Usuario de PostgreSQL (asegúrate de que sea correcto)
-  host: 'localhost',       // Si estás usando PostgreSQL localmente
+  host: 'lumedia.cmvygq66k57c.us-east-1.rds.amazonaws.com',       // RDS endpoint
   database: 'lumedia',     // Nombre de la base de datos que quieres usar
-  password: '1234',        // Contraseña que me mencionaste
-  port: 5433,              // Puerto por defecto de PostgreSQL
+  password: 'lumedia123',        // Contraseña que me mencionaste
+  port: 5432,              // Puerto por defecto de PostgreSQL
 });
 
 module.exports = pool;
