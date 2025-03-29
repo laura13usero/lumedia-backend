8a887a30c5cfe6806066a5c5249c21b94af4b940
laura13usero <laura13usero@gmail.com>
2025-03-29 18:14:15 +0100

    change bbdd

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',        // Usuario de PostgreSQL (asegúrate de que sea correcto)
   host: 'lumedia.cmvygq66k57c.us-east-1.rds.amazonaws.com',       // RDS endpoint
-  database: 'lumedia',     // Nombre de la base de datos que quieres usar
+  database: 'postgres',     // Nombre de la base de datos que quieres usar
   password: 'lumedia123',        // Contraseña que me mencionaste
   port: 5432,              // Puerto por defecto de PostgreSQL
 });
