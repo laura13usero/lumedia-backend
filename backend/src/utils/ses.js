cbfb53ea232db56f5fafe4f504ca775cdbf0cb3d
laura13usero <laura13usero@gmail.com>
2025-03-29 19:45:24 +0100

    try lambda

--- backend/src/utils/ses.js
+++ backend/src/utils/ses.js
@@ -1,5 +1,6 @@
 // Importación de SES del SDK v3
 const { SESClient, SendEmailCommand } = require('@aws-sdk/client-ses');
+const AWS = require('aws-sdk');
 
 // Configura el cliente SES con las credenciales
 const sesClient = new SESClient({
@@ -12,7 +13,7 @@ const sesClient = new SESClient({
 
 // Función para enviar el correo de confirmación
 exports.enviarCorreoConfirmacion = async (email, token) => {
-  const link = `http://3.88.175.169:3000/api/auth/confirmar/${token}`;
+  /*const link = `http://3.88.175.169:3000/api/auth/confirmar/${token}`;
 
   const params = {
     Destination: {
@@ -28,7 +29,35 @@ exports.enviarCorreoConfirmacion = async (email, token) => {
         },
       },
     },
-    Source: process.env.SES_EMAIL_FROM,
+    Source: process.env.SES_EMAIL_FROM,*/
+
+
+    
+
+    // Configura la región (si no lo tienes configurado globalmente)
+    AWS.config.update({ region: 'us-east-1' }); // Cambia la región según tu configuración
+    
+    // Crea el objeto Lambda
+    const lambda = new AWS.Lambda();
+    
+    const params = {
+      FunctionName: 'emailconfirmacion', // Reemplaza con el nombre de tu Lambda
+      //Payload: JSON.stringify({ key: 'value' }), // Los parámetros que tu Lambda espera
+    };
+    
+    lambda.invoke(params, (err, data) => {
+      if (err) {
+        console.error('Error al invocar la Lambda:', err);
+      } else {
+        console.log('Respuesta de la Lambda:', JSON.parse(data.Payload));
+      }
+    });
+
+
+
+
+
+
   };
 
   const command = new SendEmailCommand(params);
@@ -39,4 +68,4 @@ exports.enviarCorreoConfirmacion = async (email, token) => {
   } catch (err) {
     console.error('Error al enviar el correo: ', err);
   }
-};
+
