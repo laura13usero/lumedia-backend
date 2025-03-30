ccb80f0779bf0a2e9c4c6973d0561bb3bab9d82c
laura13usero <laura13usero@gmail.com>
2025-03-30 17:01:37 +0200

    test upload video

--- frontend/js/subir.js
+++ frontend/js/subir.js
@@ -1,71 +1,18 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      alert('Debes iniciar sesión');
-      window.location.href = 'login.html';
-      return;
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  // Asegúrate de que el DOM esté completamente cargado
+  alert('DOM completamente cargado');
   
-    // Cargar categorías
-    const categoriaSelect = document.getElementById('categoria-select');
-    const cats = await fetch('http://3.88.175.169:3000/api/categorias').then(r => r.json());
-    cats.forEach(cat => {
-      const option = document.createElement('option');
-      option.value = cat.id;
-      option.textContent = cat.nombre;
-      categoriaSelect.appendChild(option);
-    });
+  const form = document.getElementById('upload-form');
   
-    // Formulario
-    const form = document.getElementById('upload-form');
-    const mensaje = document.getElementById('mensaje');
+  if (!form) {
+    alert('⚠️ El formulario no existe o el ID está mal');
+  } else {
+    alert('✅ Formulario encontrado');
+  }
 
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();  // Prevenir el envío del formulario
 
-    if (!form) {
-      alert('⚠️ El formulario no existe o el ID está mal');
-    } else {
-      alert('✅ Formulario encontrado');
-    }
-  
-   /*form.addEventListener('submit', async (e) => {
-      e.preventDefault();
-      const formData = new FormData(form);
-  
-      alert('Subiendo vídeo...');
-      for (let pair of formData.entries()) {
-        alert(pair[0], pair[1]); // 👈 Verifica que los datos están correctos
-      }
-
-      try {
-        const res = await fetch('http://3.88.175.169:3000/api/videos/upload', {
-          method: 'POST',
-          headers: {
-            Authorization: 'Bearer ' + token
-          },
-          body: formData
-        });
-  
-        if (!res.ok) {
-          const err = await res.json();
-          alert('Error al subir el vídeo: ' + err + err.error);
-          throw new Error(err.error || 'Error al subir el vídeo');
-        }
-  
-        
-        alert('¡Vídeo subido correctamente!');
-        form.reset();
-        setTimeout(() => window.location.href = 'perfil.html', 1500);
-      } catch (err) {
-        alert('Error al subir el vídeo2: ' + err + err.error);
-        mensaje.textContent = `❌ ${err.message}`;
-      }
-    });*/
-
-    form.addEventListener('submit', async (e) => {
-      e.preventDefault();  // Evita que el formulario se envíe de forma tradicional
-      alert('Formulario enviado');
-      
-      // Aquí va el código que envía los datos a la API
-    });
+    alert('Formulario enviado');
   });
-  
+});
