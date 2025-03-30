82c484fd99a03cd4b8d70b60b4bc87fddcc6e5d2
laura13usero <laura13usero@gmail.com>
2025-03-30 17:05:48 +0200

    test upload video

--- frontend/js/subir.js
+++ frontend/js/subir.js
@@ -10,9 +10,37 @@ document.addEventListener('DOMContentLoaded', () => {
     alert('✅ Formulario encontrado');
   }
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();  // Prevenir el envío del formulario
+  form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const formData = new FormData(form);
+  
+      alert('Subiendo vídeo...');
+      for (let pair of formData.entries()) {
+        alert(pair[0], pair[1]); // 👈 Verifica que los datos están correctos
+      }
 
-    alert('Formulario enviado');
-  });
+      try {
+        const res = await fetch('http://3.88.175.169:3000/api/videos/upload', {
+          method: 'POST',
+          headers: {
+            Authorization: 'Bearer ' + token
+          },
+          body: formData
+        });
+  
+        if (!res.ok) {
+          const err = await res.json();
+          alert('Error al subir el vídeo: ' + err + err.error);
+          throw new Error(err.error || 'Error al subir el vídeo');
+        }
+  
+        
+        alert('¡Vídeo subido correctamente!');
+        form.reset();
+        setTimeout(() => window.location.href = 'perfil.html', 1500);
+      } catch (err) {
+        alert('Error al subir el vídeo2: ' + err + err.error);
+        mensaje.textContent = `❌ ${err.message}`;
+      }
+    });
 });
