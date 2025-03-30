3dc4aa19927e53281e20f720bc45fd55716e7bd4
laura13usero <laura13usero@gmail.com>
2025-03-30 12:37:20 +0200

    test video

--- frontend/js/perfil.js
+++ frontend/js/perfil.js
@@ -37,20 +37,38 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
   
     // Cargar todos los vídeos y filtrar por usuario
-    const todos = await fetch('http://3.88.175.169:3000/api/videos').then(r => r.json());
+    /*const todos = await fetch('http://3.88.175.169:3000/api/videos').then(r => r.json());
     const propios = todos.filter(v => v.usuario_id === userId);
-    document.getElementById('count-videos').innerText = propios.length;
-    mostrarVideos(propios, document.getElementById('mis-videos-list'));
+    document.getElementById('count-videos').innerText = propios.length;*/
+    mostrarVideos(propios, 'https://lumedia-videos.s3.us-east-1.amazonaws.com/ramen.mp4');
   });
   
   // Función para mostrar vídeos en un contenedor
-  function mostrarVideos(videos, contenedor) {
+  function mostrarVideos(videos, url) {
     contenedor.innerHTML = videos.map(video => `
       <div class="video-card">
-        <h3>${video.titulo}</h3>
-        <p>${video.descripcion}</p>
-        <a href="${video.url}" target="_blank">Ver vídeo</a>
+        <h3>titulo</h3>
+        <p>aaa</p>
+        <a href="${url}" target="_blank">Ver vídeo</a>
       </div>
     `).join('');
   }
+
+
+  /*nction mostrarVideos(videos, contenedor) {
+    contenedor.innerHTML = videos.map(video => {
+      // Aquí creamos un iframe para mostrar el video
+      const videoElement = `
+        <div class="video-card">
+          <h3>${video.titulo}</h3>
+          <p>${video.descripcion}</p>
+          <iframe src="${video.url}
+                  width="560" height="315" frameborder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowfullscreen></iframe>
+        </div>
+      `;
+      return videoElement;
+    }).join('');
+  }*/
   
