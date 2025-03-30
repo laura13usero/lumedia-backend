cfd2670e78440d9a71464c053fbb841697aed9a1
laura13usero <laura13usero@gmail.com>
2025-03-30 19:50:21 +0200

    add tokens

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -4,6 +4,8 @@ exports.verificarToken = (req, res, next) => {
   // Aseguramos que siempre pase el middleware sin hacer validación del token
   console.log('Pasando por el middleware de verificación de token');
 
+
+  req.token = token;
   // Deja pasar la petición sin verificación del token
   next();
 };
